b55500ad6a96d29bc4d3424dbe188dc7e5f5e34e
AlexeyBarabash <alexey@brave.com>
2018-10-24 21:15:31 +0300

    more bookmark_change_processor tests

--- components/brave_sync/test_util.cc
+++ components/brave_sync/test_util.cc
@@ -7,6 +7,8 @@
 #include "base/files/file_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "brave/components/brave_sync/brave_sync_service_factory.h"
+#include "brave/components/brave_sync/tools.h"
+#include "brave/components/brave_sync/values_conv.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -55,5 +57,81 @@ std::unique_ptr<KeyedService> BuildFakeBookmarkModelForTests(
       TestBookmarkClient::CreateModelWithClient(std::move(client)));
   return model;
 }
+
+SyncRecordPtr SimpleBookmarkSyncRecord(
+    const int action,
+    const std::string& object_id,
+    const std::string& location,
+    const std::string& title,
+    const std::string& order,
+    const std::string& parent_object_id) {
+  // Need:
+  // url
+  // title
+  // action
+  // order
+  // Generated:
+  // object id
+  // device id
+  auto record = std::make_unique<brave_sync::jslib::SyncRecord>();
+  record->action = ConvertEnum<brave_sync::jslib::SyncRecord::Action>(action,
+    brave_sync::jslib::SyncRecord::Action::A_MIN,
+    brave_sync::jslib::SyncRecord::Action::A_MAX,
+    brave_sync::jslib::SyncRecord::Action::A_INVALID);
+
+  record->deviceId = "3";
+  record->objectId = object_id.empty() ? tools::GenerateObjectId() : object_id;
+  record->objectData = "bookmark";
+
+  record->syncTimestamp = base::Time::Now();
+
+  auto bookmark = std::make_unique<brave_sync::jslib::Bookmark>();
+
+  bookmark->isFolder = false;
+  // empty parentFolderObjectId means child of some permanent node
+  bookmark->parentFolderObjectId = parent_object_id;
+  bookmark->hideInToolbar = true;
+  bookmark->order = order;
+
+  bookmark->site.location = location;
+  bookmark->site.title = title;
+
+  record->SetBookmark(std::move(bookmark));
+
+  return record;
+}
+
+SyncRecordPtr SimpleFolderSyncRecord(
+    const int action,
+    const std::string& title,
+    const std::string& order,
+    const std::string& parent_object_id,
+    const bool hide_in_toolbar) {
+  auto record = std::make_unique<brave_sync::jslib::SyncRecord>();
+  record->action = ConvertEnum<brave_sync::jslib::SyncRecord::Action>(action,
+    brave_sync::jslib::SyncRecord::Action::A_MIN,
+    brave_sync::jslib::SyncRecord::Action::A_MAX,
+    brave_sync::jslib::SyncRecord::Action::A_INVALID);
+
+  record->deviceId = "3";
+  record->objectId = tools::GenerateObjectId();
+  record->objectData = "bookmark";
+
+  record->syncTimestamp = base::Time::Now();
+
+  auto bookmark = std::make_unique<brave_sync::jslib::Bookmark>();
+
+  bookmark->isFolder = true;
+  bookmark->parentFolderObjectId = parent_object_id;
+  bookmark->hideInToolbar = hide_in_toolbar;
+  bookmark->order = order;
+
+  bookmark->site.title = title;
+
+  record->SetBookmark(std::move(bookmark));
+
+  return record;
+}
+
 
 }  // namespace
--- components/brave_sync/test_util.h
+++ components/brave_sync/test_util.h
@@ -60,6 +60,21 @@ std::unique_ptr<Profile> CreateBraveSyncProfile(const base::FilePath& path);
 std::unique_ptr<KeyedService> BuildFakeBookmarkModelForTests(
      content::BrowserContext* context);
 
+SyncRecordPtr SimpleBookmarkSyncRecord(
+    const int action,
+    const std::string& object_id,
+    const std::string& location,
+    const std::string& title,
+    const std::string& order,
+    const std::string& parent_object_id);
+
+SyncRecordPtr SimpleFolderSyncRecord(
+    const int action,
+    const std::string& title,
+    const std::string& order,
+    const std::string& parent_object_id,
+    const bool hide_in_toolbar);
+
 }  // namespace brave_sync
 
 #endif  // BRAVE_COMPONENTS_BRAVE_SYNC_TEST_UTIL_H_
