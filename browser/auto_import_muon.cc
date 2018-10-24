7e622d35fd565f86be1c17a3ec128230fd18ba3c
Garrett Robinson <grobinson@brave.com>
2018-10-24 12:19:32 -0700

    WIP: auto-import from Muon
    Problem: not sure how to get correct Profile in
    BraveBrowserMainExtraParts::PreMainMessageLoopRun.

--- browser/auto_import_muon.cc
+++ browser/auto_import_muon.cc
@@ -7,6 +7,10 @@
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/run_loop.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/importer/importer_list.h"
+#include "chrome/common/importer/importer_data_types.h"
 
 namespace brave {
 
@@ -18,6 +22,42 @@ void AutoImportMuon() {
     return;
 
   LOG(INFO) << "Starting auto-import from Muon";
+
+  // ImporterList::DetectSourceProfiles
+  // -> profiles_loaded_callback
+  // based on https://cs.chromium.org/chromium/src/chrome/browser/first_run/first_run.cc?l=560-577&rcl=e2569db721bb3bd6b12991d581c60933bf555f30
+  base::RunLoop run_loop;
+  auto importer_list = std::make_unique<ImporterList>();
+  importer_list->DetectSourceProfiles(
+      g_browser_process->GetApplicationLocale(),
+      false,  // include_interactive_profiles
+      run_loop.QuitClosure());
+  run_loop.Run();
+
+  bool brave_profile_found = false;
+  size_t brave_profile_index = 0;
+  for (size_t i = 0; i < importer_list->count(); i++) {
+    const auto& source_profile = importer_list->GetSourceProfileAt(i);
+    if (source_profile.importer_type == importer::TYPE_BRAVE) {
+      brave_profile_found = true;
+      brave_profile_index = i;
+      break;
+    }
+  }
+  if (!brave_profile_found) {
+    LOG(INFO) << "Could not find Muon profile to auto-import";
+    return;
+  }
+
+  const importer::SourceProfile& brave_profile =
+      importer_list->GetSourceProfileAt(brave_profile_index);
+
+  uint16_t items_to_import = 0;
+  items_to_import |= brave_profile.services_supported;
+
+  if (profile_) {
+    LOG(INFO) << "I have a profile to import from";
+  }
 }
 
 }
